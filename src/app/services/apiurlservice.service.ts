e2529ca75a90562de39906f5e4c4f8fb689ba7f8
DocN <031247ryan@gmail.com>
2018-11-27 18:30:48 -0800

    added change password to preferences

--- src/app/services/apiurlservice.service.ts
+++ src/app/services/apiurlservice.service.ts
@@ -6,8 +6,8 @@ import { Injectable } from '@angular/core';
 export class APIURLserviceService {
 
   //domains
-  //public domain = "https://localhost:44331";
-  public domain ="https://badgeapi.azurewebsites.net";
+  public domain = "https://localhost:44331";
+  //public domain ="https://badgeapi.azurewebsites.net";
   public loginURL = this.domain + "/api/auth/login";
   public userDataURL = this.domain + "/api/personalprofile";
   public editUserURL = this.domain + "/api/personalprofile/edituser";
@@ -15,5 +15,6 @@ export class APIURLserviceService {
   public editProfileDescURL = this.domain + "/api/personalprofile/editDescription";
   public searchURL = this.domain + "/api/search";
   public getProfileDataURL = this.domain + "/api/profile/";
+  public changePasswordURL = this.domain + "/api/auth/changepassword";
   constructor() { }
 }
